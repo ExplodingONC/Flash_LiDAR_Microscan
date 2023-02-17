5fb9e554a74b685cf8250aa4893d651b54ecf9a1
ExplodingONC <zhangxinyue9825@hotmail.com>
2023-02-17 18:04:52 +0800

    add iterative back projection super res algorithm

--- signal_testbench.py
+++ signal_testbench.py
@@ -2,20 +2,24 @@ import numpy as np
 import SensorSignal
 import superRes
 
-depth_map = (np.arange(16 * 24) / 4 + 1).reshape((16, 24))
+# depth_map = (np.arange(12 * 16) / 10 + 1).reshape((12, 16))
+depth_strip = np.block([np.ones([12,1]),np.ones([12,7])*10])
+depth_map = np.block([depth_strip,depth_strip])
+
 print(depth_map)
 
-sig = []
+sigs = []
 for i in range(4):
-    sig.append(SensorSignal.SensorSignal([8, 12], downsample_ratio=2))
-    sig[-1].T_0 = 8 / 60 * 1e-6
+    sigs.append(SensorSignal.SensorSignal([6, 8], downsample_ratio=2))
+    sigs[-1].set_timing(8 / 60 * 1e-6)
 
-sig[0].sim_data(depth_map, downsample_ratio=2, shift_vec=[0, 0])
-sig[1].sim_data(depth_map, downsample_ratio=2, shift_vec=[0.5, 0])
-sig[2].sim_data(depth_map, downsample_ratio=2, shift_vec=[0, 0.5])
-sig[3].sim_data(depth_map, downsample_ratio=2, shift_vec=[0.5, 0.5])
+sigs[0].sim_data(depth_map, downsample_ratio=2, shift_vec=[0, 0])
+sigs[1].sim_data(depth_map, downsample_ratio=2, shift_vec=[0.5, 0])
+sigs[2].sim_data(depth_map, downsample_ratio=2, shift_vec=[0, 0.5])
+sigs[3].sim_data(depth_map, downsample_ratio=2, shift_vec=[0.5, 0.5])
 
-sig_sr = superRes.linear(sig[0],sig[1],sig[2],sig[3])
+np.set_printoptions(formatter={'float': lambda x: "{0:5.2f}".format(x)})
+sig_sr = superRes.iterative(*sigs)
 
 distance = sig_sr.calc_dist()
 print(distance)
--- superRes.py
+++ superRes.py
@@ -1,25 +1,65 @@
+import copy
 import numpy as np
 import SensorSignal
 
 
-def linear(*sig: SensorSignal):
+def linear(*sigs: SensorSignal):
     # basic info
-    sr_res = sig[0].resolution * 2
-    sr_downsample_ratio = sig[0].downsample_ratio / 2
+    sr_res = sigs[0].resolution * 2
+    sr_downsample_ratio = sigs[0].downsample_ratio / 2
     sr_sig = SensorSignal.SensorSignal(sr_res, sr_downsample_ratio)
-    sr_sig.shift_vector = sig[0].shift_vector
-    sr_sig.set_timing(sig[0].T_0)
+    sr_sig.shift_vector = sigs[0].shift_vector
+    sr_sig.set_timing(sigs[0].T_0)
     # linear super res
     for subframe in range(4):
         for channel in range(2):
-            for frame in range(4):
-                slice = sig[frame].data[subframe, channel, :, :]
+            for k, signal in enumerate(sigs):
+                slice = signal.data[subframe, channel, :, :]
                 slice = SensorSignal.rebin(slice, sr_res)
-                slice = SensorSignal.translate(slice, 2 * sig[frame].shift_vector)
+                slice = SensorSignal.translate(slice, 2 * signal.shift_vector)
                 sr_sig.data[subframe, channel, :, :] += slice
-            sr_sig.data[subframe, channel, :, :] = sr_sig.data[subframe, channel, :, :] / 4
+            sr_sig.data[subframe, channel, :, :] = sr_sig.data[subframe, channel, :, :] / (k + 1)
     return sr_sig
 
 
-def iterative():
-    pass # WIP
+def back_projection(sig_ref: SensorSignal, sig_iter: SensorSignal):
+    dist_ref = sig_ref.calc_dist()
+    dist_iter = sig_iter.calc_dist()
+    # valid if both returns a valid distance
+    valid_mask = np.logical_and(np.isfinite(dist_ref), np.isfinite(dist_iter))
+    # need compensation if reference is out of range but iteration still valid
+    compensate_mask = np.logical_and(~np.isfinite(dist_ref), np.isfinite(dist_iter))
+    # generate difference for iteration
+    dist_diff = dist_ref - dist_iter
+    dist_diff[~valid_mask] = 0
+    dist_diff[compensate_mask] = np.inf
+    return dist_diff
+
+
+def iterative(*sigs_ref: SensorSignal, iter_cnt=25):
+    # prepare iteration start point
+    sig_cnt = len(sigs_ref)
+    depth_iter = linear(*sigs_ref).calc_dist()
+    # prepare iteration vals (must use deep copy for objects)
+    sigs_iter = copy.deepcopy(sigs_ref)
+    # iteration
+    for iter in range(iter_cnt):
+        # prepare error log
+        err_total = np.zeros(depth_iter.shape)
+        for sig_ref, sig_iter in zip(sigs_ref, sigs_iter):
+            # simulate projected signal
+            sig_iter.sim_data(depth_iter, downsample_ratio=2,
+                              shift_vec=sig_iter.shift_vector - sigs_iter[0].shift_vector)
+            # back projection
+            err_iter = back_projection(sig_ref, sig_iter)
+            err_iter = SensorSignal.rebin(err_iter, sig_iter.resolution * 2)
+            err_iter = SensorSignal.translate(err_iter, sig_iter.shift_vector * 2)
+            # update error log
+            err_total = err_total + err_iter
+        # update iterative depth map
+        err_total = err_total / sig_cnt
+        depth_iter = depth_iter + err_total * 0.5
+    # generate result
+    sig_ret = SensorSignal.SensorSignal(depth_iter.shape)
+    sig_ret.sim_data(depth_iter, downsample_ratio=1, shift_vec=[0, 0])
+    return sig_ret
