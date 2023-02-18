ef1917370cc369af558e2a924f20162b3fb348b6
ExplodingONC <zhangxinyue9825@hotmail.com>
2023-02-18 21:07:56 +0800

    re-organize figure display

--- signal_testbench.py
+++ signal_testbench.py
@@ -25,23 +25,26 @@ sigs[3].sim_data(depth_map, downsample_ratio=2, shift_vec=[0.5, 0.5])
 print("simulation done.")
 
 np.set_printoptions(formatter={'float': lambda x: "{0:5.2f}".format(x)})
-sig_sr = superRes.iterative(*sigs, iter_cnt=25, term_cond=5e-3)
-distance = sig_sr.calc_dist()
-intensity = sig_sr.calc_intensity()
+sig_sr_linear = superRes.linear(*sigs)
+sig_sr_ibp = superRes.iterative(*sigs, iter_cnt=25, term_cond=5e-3)
 print("calculation done.")
 
 mpl.rcParams['image.interpolation'] = 'none'
 # print depth_map
 im_depth = axs[0, 0].imshow(depth_map, cmap='viridis_r', vmin=0, vmax=15)
 fig.colorbar(im_depth,ax=axs[0, 0])
+axs[0, 0].set_title("Reference")
 # print raw data
-im_raw = axs[1, 0].imshow(sigs[0].calc_dist(), cmap='viridis_r', vmin=0, vmax=15)
-fig.colorbar(im_raw,ax=axs[1, 0])
-# print super-res distance
-im_dist = axs[0, 1].imshow(distance, cmap='viridis_r', vmin=0, vmax=15)
-fig.colorbar(im_dist,ax=axs[0, 1])
-# print super-res intensity
-im_inten = axs[1, 1].imshow(intensity, cmap='inferno', vmin=0, vmax=np.max(intensity))
-fig.colorbar(im_inten,ax=axs[1, 1])
+im_raw = axs[0, 1].imshow(sigs[0].calc_dist(), cmap='viridis_r', vmin=0, vmax=15)
+fig.colorbar(im_raw,ax=axs[0, 1])
+axs[0, 1].set_title("Raw data")
+# print super-res distance (linear)
+im_dist = axs[1, 0].imshow(sig_sr_linear.calc_dist(), cmap='viridis_r', vmin=0, vmax=15)
+fig.colorbar(im_dist,ax=axs[1, 0])
+axs[1, 0].set_title("Linear super-res")
+# print super-res distance (iterative)
+im_dist = axs[1, 1].imshow(sig_sr_ibp.calc_dist(), cmap='viridis_r', vmin=0, vmax=15)
+fig.colorbar(im_dist,ax=axs[1, 1])
+axs[1, 1].set_title("IBP super-res")
 print("display done.")
 plt.show()
