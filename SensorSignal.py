710b85cd0cb1435ca6d48a2f6b068e337b5e5dd3
ExplodingONC <zhangxinyue9825@hotmail.com>
2023-03-24 17:16:35 +0800

    seperate distance calculation for actual and simulated data

--- SensorSignal.py
+++ SensorSignal.py
@@ -143,15 +143,16 @@ class SensorSignal:
     # calculate deltas
     def calc_delta(self):
         # delta_F1 = (F1_Ch2 + F3_Ch1) - (F1_Ch1 + F3_Ch2)
-        self.delta_F1 = (self.data[0, 1, :, :] + self.data[2, 0, :, :]) * 1.05 \
+        self.delta_F1 = (self.data[0, 1, :, :] + self.data[2, 0, :, :]) \
             - (self.data[0, 0, :, :] + self.data[2, 1, :, :])
         # delta_F2 = (F2_Ch2 + F4_Ch1) - (F2_Ch1 + F4_Ch2)
-        self.delta_F2 = (self.data[1, 1, :, :] + self.data[3, 0, :, :]) * 1.05 \
+        self.delta_F2 = (self.data[1, 1, :, :] + self.data[3, 0, :, :]) \
             - (self.data[1, 0, :, :] + self.data[3, 1, :, :])
 
     # extract distance
     def calc_dist(self):
         self.calc_delta()
+        np.seterr(invalid='ignore')
         # mask for valid data (non-zero)
         if np.any(np.abs(self.delta_F1) > 1) or np.any(np.abs(self.delta_F2) > 1):
             # for actual 12-bit uint data (which has noise)
@@ -159,20 +160,28 @@ class SensorSignal:
                 (np.abs(self.delta_F1) >= 10),
                 (np.abs(self.delta_F2) >= 10)
             )
+            depth_map \
+                = (self.delta_F1 > 0) \
+                * (self.delta_F2 / (abs(self.delta_F1) + abs(self.delta_F2)) + 1) \
+                / 4 * const.speed_of_light * self.T_0 \
+                + (self.delta_F1 <= 0) \
+                * (-self.delta_F2 / (abs(self.delta_F1) + abs(self.delta_F2)) + 3) \
+                / 4 * const.speed_of_light * self.T_0
+            depth_map[~valid_mask] = np.inf
+            depth_map = np.nan_to_num(depth_map, copy=False, nan=np.inf, posinf=np.inf, neginf=np.inf)
         else:
             # for simulation data
             valid_mask = np.logical_or(
                 (np.abs(self.delta_F1) > 0),
                 (np.abs(self.delta_F2) > 0)
             )
-        np.seterr(invalid='ignore')
-        depth_map \
-            = (self.delta_F1 > 0) \
-            * (self.delta_F2 / (abs(self.delta_F1) + abs(self.delta_F2)) + 1) \
-            / 4 * const.speed_of_light * self.T_0 \
-            + (self.delta_F1 <= 0) \
-            * (-self.delta_F2 / (abs(self.delta_F1) + abs(self.delta_F2)) + 3) \
-            / 4 * const.speed_of_light * self.T_0
-        depth_map[~valid_mask] = np.inf
-        depth_map = np.nan_to_num(depth_map, copy=False, nan=np.inf, posinf=np.inf, neginf=np.inf)
+            depth_map \
+                = (self.delta_F1 > 0) \
+                * (self.delta_F2 / (abs(self.delta_F1) + abs(self.delta_F2)) + 1) \
+                / 4 * const.speed_of_light * self.T_0 \
+                + (self.delta_F1 <= 0) \
+                * (-self.delta_F2 / (abs(self.delta_F1) + abs(self.delta_F2)) + 3) \
+                / 4 * const.speed_of_light * self.T_0
+            depth_map[~valid_mask] = np.inf
+            depth_map = np.nan_to_num(depth_map, copy=False, nan=np.inf, posinf=np.inf, neginf=np.inf)
         return depth_map
